c2d77b662f16d14bdeb34c486bf2d55e178ca6a2
Ruslan <ruslanpidhainyi02@gmail.com>
2025-02-13 17:21:30 +0100

    Add project files.

---
+++ eBook-.Net9-MVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using eBook_.Net9_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eBook_.Net9_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ eBook-.Net9-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eBook_.Net9_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
