121d4043d85b53425e4157dbddddbd91e667cb39
Ruslan <ruslanpidhainyi02@gmail.com>
2025-02-14 03:15:07 +0100

    Class Entities

--- eBook-.Net9-MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace eBook_.Net9_MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
